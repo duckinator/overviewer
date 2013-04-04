3af38d0b40faafb98744b46dddd7b17622f39ef2
Nick Markwell <nick@duckinator.net>
2013-04-04 05:09:39 -0400

    Decent start on integrating overviewer.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
-source :rubygems
+source 'https://rubygems.org'
 
 gem 'rack',     '~> 1.4.1'
 gem 'sinatra',  '~> 1.3.3'
 #gem 'liquid',   '~> 2.4.1'
 gem 'mustache', '~> 0.99.4'
+
+gem 'duck-duck-go'
+
+gem 'dbpedia'
+gem 'sparql-client' # Required by dbpedia
--- views/overviewer.rb
+++ views/overviewer.rb
@@ -1,7 +1,10 @@
+require 'dbpedia'
+require 'duck_duck_go'
+
 class Inatri
   module Views
     class Overviewer < Layout
-      attr_accessor :query
+      attr_accessor :query, :abstract, :links, :related
 
       def title
         @query || false
@@ -11,9 +14,98 @@ class Inatri
         'overviewer'
       end
 
-      def search?
-        true
+      #def search?
+      #  true
+      #end
+
+      def abstract?
+        _ddg_fetch
+
+        !!@abstract
       end
+
+      def links?
+        _ddg_fetch
+
+        !!@links
+      end
+
+      def related?
+        _ddg_fetch
+
+        !!@related
+      end
+
+      # Fetch from DuckDuckGo
+      def _ddg_fetch
+        return if @fetched
+        @fetched = true
+
+        ddg = DuckDuckGo.new
+        zci = ddg.zeroclickinfo(@query)
+
+        @abstract = {
+          text:   zci.abstract_text,
+          url:    zci.abstract_url,
+          source: zci.abstract_source,
+        }
+
+        @abstract = nil if @abstract[:text].nil? || @abstract[:text].empty?
+
+        topics = zci.related_topics["_"]
+
+        @related = _format_related(topics) if topics
+      end
+
+      def _format_related(topics)
+        handle_topics(query, topics, @category ? 'category' : nil)
+      end
+
+
+
+  def handle_topics(query, topics, type)
+    ret = []
+
+    topics.each do |topic|
+      if topic.is_a?(Hash) && topic.include?('Topics')
+        # This returns something of the format:
+        # {"Topics"=>
+        #   [{"Result"=>
+        #      ...},
+        #    <snip>
+        #    {"Result"=>
+        #      ...}],
+        #  "Name"=>"Category"}
+        #
+        # This should probably use 'Name', but it doesn't for now.
+        ret += handle_topics(query, topic.topics, type)
+        next
+      end
+
+      icon = topic.icon
+      result = topic.result
+
+      result.gsub!('<a href="http://duckduckgo.com/c/', '<a href="/overviewer?type=category&amp;q=')
+      result.gsub!('<a href="http://duckduckgo.com/', '<a href="/overviewer?q=')
+      result.gsub!('_','+')
+
+      result.gsub!('?q=?q=', '?q=') # Is this because of 3 lines up?
+
+      result_query = result.split('<a href="')[-1].split('">')[0]
+
+      query_hash = CGI::parse(URI.parse(URI.encode(result_query)).query)
+      result_type = (query_hash['type'] || ['']).last
+
+      if (CGI.escape(query).downcase == result_query.downcase) && (result_type == type)
+        next
+      end
+
+      ret << { icon: icon.url, result: result }
+    end
+
+    ret
+  end
+
     end
   end
 end
